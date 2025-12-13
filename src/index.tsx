98cb46303103318abcc4579286749d0f06a16c11
priz80 <priz8090@gmail.com>
2025-12-13 21:45:33 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import { ToDoList } from './pages/ToDoList';
+import './assets/scss/normalize.scss';
+import './assets/scss/style.scss';
+import { ToDoListPage } from './pages/ToDoListPage';
 
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <ToDoListPage />
   </React.StrictMode>
 );
