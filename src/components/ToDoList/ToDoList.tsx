bc9d98b568f0d081ae398ff91d3e108ad2463624
priz80 <priz8090@gmail.com>
2025-12-13 22:01:43 +0300

    Update ToDoList.tsx

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,7 +1,19 @@
 import { ToDoListItem } from "./ToDoListItem/ToDoListItem"
 import './ToDoList.scss'
 
+interface ToDo {
+  id: number,
+  text: string,
+  isDone: boolean
+}
+
 export const ToDoList = () => {
+  const todo1: ToDo = {
+    id: 0,
+    text: 'Первая задача',
+    isDone: false
+  }
+  
     return  (
         <div className="todo-container">
           <ul className="todo-list failed">
