2f73f1ab90612aba4e28197f747b69586e4be0ad
priz80 <priz8090@gmail.com>
2025-12-14 02:57:40 +0300

    Update ToDoListPage.tsx

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,6 +3,8 @@ import { Form } from "../components/Form/Form"
 import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
 import { ToDo } from "../models/todo-item"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export const ToDoListPage = () => {
   const [todos, setTodos] = useState<ToDo[]>([])
@@ -13,7 +15,8 @@ export const ToDoListPage = () => {
       text: text,
       isDone: false,
     }
-    setTodos ([...todos, newToDo])
+    setTodos([...todos, newToDo])
+    toast.success("Задача добавлена!")
   }
 
   const updateToDo = (toDoItem: ToDo) => {
@@ -24,21 +27,22 @@ export const ToDoListPage = () => {
       return todo
     })
     setTodos(newTodos)
+    const status = toDoItem.isDone ? "выполнена" : "отмечена как невыполненная"
+    toast.info(`Задача "${toDoItem.text}" ${status}`)
   }
 
   const deleteToDo = (toDoItem: ToDo) => {
     const newTodos = todos.filter((todo) => todo.id !== toDoItem.id)
     setTodos(newTodos)
+    toast.warn("Задача удалена")
   }
-  
 
   return (
     <>
-        <Header />
-        <Form createNewToDo={createNewToDo} />
-        <ToDoList todos={todos}  updateToDo={updateToDo} deleteToDo={deleteToDo} />
+      <Header />
+      <Form createNewToDo={createNewToDo} />
+      <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </>
-  );
-};
-
-
+  )
+}
