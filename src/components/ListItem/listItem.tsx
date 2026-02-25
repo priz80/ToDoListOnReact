f3c1312936f175964bc6cd75b2c5c8434c6ceb23
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-25 23:05:12 +0300

    Create listItem.tsx

---
+++ src/components/ListItem/listItem.tsx
@@ -0,0 +1,14 @@
+import { ToDo } from "../../models/todo-item";
+import classes from "./ListItem.module.scss";
+
+export const ListItem = ({ todo }: { todo: ToDo }) => {
+  return (
+    <a 
+      className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`}
+      target="_blank"
+      href={`/ToDoListOnReact/list/${todo.id}`}
+    >
+      {todo.text}
+    </a>
+  );
+};
