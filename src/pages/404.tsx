ea8edd9d07d806dfe16abd931dba2b8ff4db8431
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-25 23:05:22 +0300

    Create 404.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const NoteFound = () => {
+  return (
+    <div className="container">
+      <h1>Not Found</h1>
+      <Link to="/">To Home</Link>
+    </div>
+  );
+};
