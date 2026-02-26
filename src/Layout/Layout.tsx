8a32b93c52fc2ecab247e0cc505e673724d2e875
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-26 10:15:26 +0300

    Update Layout.tsx

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,41 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header/Header";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 
 export const Layout = () => {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
-};
+  <>
+    <HelmetProvider>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="description"
+            content="Web site created using create-react-app"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+          <title>ToDo List App</title>
+        </Helmet>
+    </HelmetProvider>
+        <Header />
+        <Outlet />
+  </>
+)};
+
+
+
+// export const Layout = () => {
+//   return (
+//     <>
+//       <app />
+//     </>
+//   );
+// };
