c3615e711ecde391d9311a00bdd0d8da1fe598d0
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-26 11:11:54 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <ItemDescription todos={todos} />
       },
       {
-        path: '*',
+        path: '/*',
         element: <NoteFound />
       }
     ]
